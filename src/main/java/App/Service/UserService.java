f1f97d707b07f0ef6262f2f8db12e2717c7fd3e7
Kainoa Aquino <kainoa104@gmail.com>
2022-09-30 11:22:00 -0700

    added user stuff

---
+++ src/main/java/App/Service/UserService.java
@@ -0,0 +1,34 @@
+package App.Service;
+
+
+import App.Model.Users;
+import App.Repository.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserService {
+
+    UserRepository ur;
+    @Autowired
+    public UserService(UserRepository ur) {
+        this.ur = ur;
+
+    }
+
+    public List<Users> getAllUsers() {
+
+        return ur.findAll();
+    }
+    public Users getUserById(int id){
+        return ur.findById(id).get();
+    }
+
+    public Users addUser(Users p){
+        return ur.save(p);
+    }
+}
