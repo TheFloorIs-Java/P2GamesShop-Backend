c1605e67924d6cae556c641fa52ed346751391b4
Jamie <jnguyen127@horizon.csueastbay.edu>
2022-10-31 13:30:10 -0700

    some javadocs

--- src/main/java/App/Controller/TransactionController.java
+++ src/main/java/App/Controller/TransactionController.java
@@ -18,7 +18,11 @@ public class TransactionController {
         this.ts = ts;
     }
 
-    // Get all Transactions by a specific user
+    /**
+     * Get all Transactions by a specific user
+     * @param user_id an integer representing the id of the user in the database
+     * @return a list of transactions made by a certain user
+     */
     @GetMapping("/{user_id}/transactions")
     public List<Transactions> getAllTransactionsByUserId(@PathVariable int user_id) {
         return ts.getAllTransactionsFromUserById(user_id);
@@ -26,13 +30,23 @@ public class TransactionController {
     //Service call to get all transactions based on the user ID passed in by the frontend.
 
     // Add a transaction
+
+    /**
+     *
+     * @param t
+     * @return
+     */
     @PostMapping("/{user_id}/transactions")
     public Transactions addTransaction(@RequestBody Transactions t) {
         return ts.addTransaction(t);
     }
     //Service call to add a new transaction to the database.
 
-    // Get all Transactions for a specific date
+    /**
+     * Get all Transactions for a specific date
+     * @param date A string representation of the date - FORMAT: MMDDYY
+     * @return a list of transactions on a certain date
+     */
     @GetMapping("/transactions/{date}")
     public List<Transactions> getTransactionsByDate(@PathVariable String date) {
         return ts.getTransactionsByDate(LocalDate.parse(date));
@@ -40,10 +54,19 @@ public class TransactionController {
     //Service call to get all transactions based on the date passed in by the frontend.
 
     // Testing endpoints
+
+    /**
+     * For testing purposes
+     * @return A default response
+     */
     @GetMapping("")
     public String defaultResponse() {return "Response from backend.";}
     //A default endpoint to check if the API is currently running.
 
+    /**
+     * Gets all transactions from all users
+     * @return A list of transaction from all users
+     */
     @GetMapping("/transactions")
     public List<Transactions> getAllTransactions() {
         return ts.getAllTransactions();
--- src/main/java/App/Repository/TransactionsRepository.java
+++ src/main/java/App/Repository/TransactionsRepository.java
@@ -9,19 +9,35 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionsRepository extends JpaRepository<Transactions, Integer> {
-    // For testing with Postman - gets all transactions
+    /**
+     * Queries the transactions table for all transactions - for testing purposes
+     * @return all transactions from the table
+     */
     @Query("from Transactions")
     public List<Transactions> getAllTransactions();
 
-    // All transaction from a user
+    /**
+     * Queries the transactions table for all transactions made by a user
+     * @param user_id
+     * @return a list of transactions that match the input id
+     */
     @Query("from Transactions where user_id = :user_id")
     public List<Transactions> getAllTransactionsByUserId(@Param("user_id")int user_id);
 
-    // Single transaction from a user
+    /**
+     * Queries the transaction table for a single transaction from a user
+     * @param user_id
+     * @param transaction_id
+     * @return a single transaction that match the input user id and transaction id
+     */
     @Query("from Transactions where user_id = :user_id and transaction_id = :transaction_id")
     public Transactions getTransactionFromUserById(@Param("user_id")int user_id, @Param("transaction_id")int transaction_id);
 
-    // All transactions from all users on specific date
+    /**
+     * Queries the transactions table for a list transactions made on a certain date
+     * @param date
+     * @return a list of transactions that match the input date
+     */
     @Query("from Transactions where date = :date")
     public List<Transactions> getTransactionsByDate(@Param("date")LocalDate date);
 }
